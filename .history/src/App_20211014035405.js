5019d953c4036442d2e4153a3deafdb707c263ea
souleymane kone <desolo74@gmail.com>
2021-10-14 04:11:13 -0700

    create a 404 page

--- .history/src/App_20211014035405.js
+++ .history/src/App_20211014035405.js
@@ -5,7 +5,7 @@ import Home from './components/pages/Home';
 import User from './components/users/User';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
-import NotFound from './components/pages/NotFound'
+import NotFound from './components'
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
 import './App.css';
@@ -24,7 +24,6 @@ const App = ()=> {
            < Route  exact path='/' component={Home} />
            <Route exact path='/about'  component={About} />
            <Route exact path='/user/:login' component={User} />
-           <Route component={NotFound}/>
          </Switch>
       
       </div>
---
+++ .history/src/components/pages/NotFound_20211014035233.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1> Not Found</h1>
+            <p className=''>The page you </p>
+
+        </div>
+    )
+}
+
+export default NotFound
